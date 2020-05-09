051ddd588229bbd04334cd601e94d7c796642a97
Sheri Rhoades <sheri_rhoades@yahoo.com>
2020-05-09 16:30:32 -0500

    add manifest

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -5,7 +5,8 @@ const FILES_TO_CACHE = [
   "/assets/css/style.css",
   "/assets/js/db.js",
   "/assets/js/index.js",
-  "/assets/images/budget.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-512x512.png",
 ];
 
 const CACHE_NAME = "static-cache-v2";
